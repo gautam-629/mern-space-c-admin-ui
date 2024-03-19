99cf9eacb7280a7f4be3989d286dfeb93fc6a9a3
gautam-629 <gautam629@gmail.com>
2024-03-19 09:47:00 +0545

    add login and logout function

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,97 +1,138 @@
-import { Layout, Card, Space, Form, Input, Checkbox, Button, Flex} from 'antd';
+import { Layout, Card, Space, Form, Input, Checkbox, Button, Flex } from 'antd';
 import { LockFilled, UserOutlined, LockOutlined } from '@ant-design/icons';
+import { useMutation, useQuery } from '@tanstack/react-query';
+import { login, logout, self } from '../../http/api';
 import Logo from '../../components/icons/Logo';
-import { useMutation } from '@tanstack/react-query';
-import { login } from '../../http/api';
 import { Credentials } from '../../type';
+import {useAuthStore} from "../../store";
+import { userPermission } from '../../hooks/usePermission';
+
 
 const loginUser = async (credentials: Credentials) => {
     const { data } = await login(credentials);
     return data;
 };
+
+const getSelf = async () => {
+    const { data } = await self();
+    return data;
+};
+
+
 const LoginPage = () => {
+
+    const {isAllowed}=userPermission();
+    const {setUser,logout:logoutFromStore}= useAuthStore();
+
+    const { refetch } = useQuery({
+        queryKey: ['self'],
+        queryFn: getSelf,
+        enabled: false,
+    });
+
+    const {mutate:logoutMutate}=useMutation({
+        mutationKey:['logout'],
+        mutationFn:logout,
+        onSuccess: ()=>{
+            logoutFromStore()
+            return;
+        }
+    })
+
     const { mutate, isPending, isError, error } = useMutation({
-        mutationKey: ['login'],
+        mutationKey:['login'],
         mutationFn: loginUser,
         onSuccess: async () => {
-            console.log('Login successful.');
+            const selfDataPromise = await refetch();
+            // logout or redirect to client ui
+            // window.location.href = "http://clientui/url"
+            // "admin", "manager", "customer"
+         
+            if(!isAllowed(selfDataPromise?.data)){
+                logoutMutate();
+                return;
+            }
+
+            setUser(selfDataPromise.data);
         },
+
     });
 
     return (
         <>
-        <Layout style={{ height: '100vh', display: 'grid', placeItems: 'center' }}>
-            <Space direction="vertical" align="center" size="large">
-                <Layout.Content
-                    style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-                    <Logo />
-                </Layout.Content>
-                <Card
-                    bordered={false}
-                    style={{ width: 300 }}
-                    title={
-                        <Space
-                            style={{ width: '100%', fontSize: 16, justifyContent: 'center' }}>
-                            <LockFilled />
-                            Sign in
-                        </Space>
-                    }>
-                    <Form
-                     onFinish={(values) => {
-                        mutate({ email: values.username, password: values.password });
-                        console.log(values);
-                    }}
+            <Layout style={{ height: '100vh', display: 'grid', placeItems: 'center' }}>
+                <Space direction="vertical" align="center" size="large">
+                    <Layout.Content
+                        style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                        <Logo />
+                    </Layout.Content>
+                    <Card
+                        bordered={false}
+                        style={{ width: 300 }}
+                        title={
+                            <Space
+                                style={{ width: '100%', fontSize: 16, justifyContent: 'center' }}>
+                                <LockFilled />
+                                Sign in
+                            </Space>
+                        }>
+                        <Form
+                            onFinish={(values) => {
+                                mutate({ email: values.username, password: values.password });
+                            }}
 
-                    >
-                    <Form.Item
-                             name="username"
-                             rules={[
-                                {
-                                    required: true,
-                                    message: 'Please input your Username',
-                                },
-                                {
-                                    type: 'email',
-                                    message: 'Email is not valid',
-                                },
-                            ]}
-                           >
-                            <Input prefix={<UserOutlined />} placeholder="Username" />
-                        </Form.Item>
-                        <Form.Item
-                            name="password"
-                            rules={[
-                                {
-                                    required: true,
-                                    message: 'Please input your password',
-                                },
-                            ]}
-                           >
-                            <Input.Password prefix={<LockOutlined />} placeholder="Password" />
-                        </Form.Item>
-                        <Flex justify="space-between">
-                            <Form.Item name="remember" valuePropName="checked">
-                                <Checkbox>Remember me</Checkbox>
+                        >
+                            <Form.Item
+                                name="username"
+                                rules={[
+                                    {
+                                        required: true,
+                                        message: 'Please input your Username',
+                                    },
+                                    {
+                                        type: 'email',
+                                        message: 'Email is not valid',
+                                    },
+                                ]}
+                            >
+                                <Input prefix={<UserOutlined />} placeholder="Username" />
                             </Form.Item>
-                            <a href="" id="login-form-forgot">
-                                Forgot password
-                            </a>
-                        </Flex>
-                        <Form.Item>
-                            <Button
-                                type="primary"
-                                htmlType="submit"
-                                style={{ width: '100%' }} >
-                                Log in
-                            </Button>
-                        </Form.Item>
-                    </Form>
+                            <Form.Item
+                                name="password"
+                                rules={[
+                                    {
+                                        required: true,
+                                        message: 'Please input your password',
+                                    },
+                                ]}
+                            >
+                                <Input.Password prefix={<LockOutlined />} placeholder="Password" />
+                            </Form.Item>
+                            <Flex justify="space-between">
+                                <Form.Item name="remember" valuePropName="checked">
+                                    <Checkbox>Remember me</Checkbox>
+                                </Form.Item>
+                                <a href="" id="login-form-forgot">
+                                    Forgot password
+                                </a>
+                            </Flex>
+                            <Form.Item>
+                                <Button
+                                    loading={isPending}
+                                    type="primary"
+                                    htmlType="submit"
+                                    style={{ width: '100%' }} >
+                                    Log in
+                                </Button>
+                            </Form.Item>
+                        </Form>
 
-                </Card>
-            </Space>
-        </Layout>
-    </>
+                    </Card>
+                </Space>
+            </Layout>
+        </>
     )
 }
 
 export default LoginPage;
+
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,27 @@
+// /// <reference types="vitest" />
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   test: {
+//     environment: 'jsdom',
+//     setupFiles: './setupTest.ts',
+//     globals: true
+//   },
+// })
+
 /// <reference types="vitest" />
 
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-// https://vitejs.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
   test: {
     environment: 'jsdom',
-    setupFiles: './setupTest.ts',
+    setupFiles: ['./setupTest.ts'], // Corrected to an array
     globals: true
-  },
-})
+  }
+});
